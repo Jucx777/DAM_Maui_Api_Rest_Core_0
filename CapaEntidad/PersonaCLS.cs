881a7efa3e5438134717ca383a19bd13355a3973
jcpap <jcpap@JUCX>
2025-06-11 11:34:10 -0600

    Versión 01: Capa Api, Capa Entidad, Prueba de Dependencia

---
+++ CapaEntidad/PersonaCLS.cs
@@ -0,0 +1,21 @@
+﻿namespace CapaEntidad
+{
+    public class PersonaCLS
+    {
+        // Listado
+
+        public int iidpersona { get; set; }
+        public string nombrecompleto { get; set; }
+        public string correo { get; set; }
+        public string fechanacimientocadena { get; set; }
+
+        // Recuperar
+
+        public string nombre { get; set; }
+        public string appaterno { get; set; }
+        public string apmaterno { get; set; }
+        public DateTime fechanacimiento { get; set; }
+        public int iisexo { get; set; }
+
+    }
+}
